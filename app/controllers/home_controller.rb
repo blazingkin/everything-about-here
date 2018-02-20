a6100705ee341189a6686560aa66e303f85e934e
Alex Gravenor <blazingkin@gmail.com>
2018-02-20 13:03:17 -0800

    Get location from IP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,6 +16,16 @@ class HomeController < ApplicationController
         end
     end
 
+    def from_ip
+        data = {}
+        data[:ip_address] = Rails.env.development? ? '207.62.170.213' : request.ip
+        @service = DataService.new
+        @service.setup
+        @service.fetch_lat_long(data)
+        @data = @service.get_data_from_services(data)
+        render 'index'
+    end
+
     private
         def has_geo_data?
             params.key?(:lat) && params.key?(:long)
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   root 'home#index'
+  get '/from_ip', to: 'home#from_ip'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
